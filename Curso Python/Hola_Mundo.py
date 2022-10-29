abf66cf555e2043229c012b2fb159503a078c7c1
YaloreangyeM <116961514+YaloreangyeM@users.noreply.github.com>
2022-10-29 11:30:35 -0500

    Primer carpeta

---
+++ Curso Python/Hola_Mundo.py
@@ -0,0 +1,10 @@
+# Ejemplo 1. SALUDO
+# COMENTARIOS: Sirven para documentar o detallar cada parte del código
+# A los comentarios no los toma en cuenta o los omite Python
+
+# Imprimiendo o mostrando mensajes o resultados
+print("Hola Mundo")
+print('Angélica');
+
+print(10+5)
+
